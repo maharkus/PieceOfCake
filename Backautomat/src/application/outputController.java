cc5d502655f832ee8ad985b93f9c0be49eb98942
julz <julz@localhost>
2021-06-10 23:58:18 +0200

    output controller

---
+++ Backautomat/src/application/outputController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class outputController {
+
+    @FXML
+    private Button backToMenu;
+    
+    public void handleBtBackToMenu() throws Exception {
+    	Parent root = FXMLLoader.load(getClass().getResource("paymentMethods.fxml"));
+    	
+    	Stage window = (Stage) backToMenu.getScene().getWindow();
+    	window.setScene(new Scene(root, 1920, 1080));
+    }
+
+}
