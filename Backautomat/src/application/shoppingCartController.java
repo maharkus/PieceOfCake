e37b9638ba7512f3dc1cad236e2844cf2cbf19e1
Titus Hellinge <titus@hellinge.com>
2021-06-24 12:53:16 +0200

    gesamtBetrag

--- Backautomat/src/application/shoppingCartController.java
+++ Backautomat/src/application/shoppingCartController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import application.classes.Product;
+import application.classes.ShoppingCartProduct;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -45,6 +46,9 @@ public class shoppingCartController {
 
     @FXML
 	private ScrollPane productWrap;
+    
+    @FXML
+    private Text gesamtBetrag;
     
 
 	private int row;
@@ -145,9 +149,23 @@ public class shoppingCartController {
 		}
 		productAmountText.setText(String.valueOf(amount));
 	}
+	
+	public void setTotal(Text g) {
+		Double total = 0.00;
+		for(ShoppingCartProduct p : Main.shoppingCart) {
+			total += p.getAmount()*p.getPreis();
+		}
+
+		Locale locale = Locale.GERMANY;
+		NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+		String euroPrice = numberFormat.format(total);
+		g.setText(euroPrice);
+		
+	}
 
     
     public void initialize() throws IOException {
 		shoppingCartGrid.add(createGrid(), 0, row);
+		setTotal(gesamtBetrag);
 	}
 }
